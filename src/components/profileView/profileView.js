519298536ebf89f0a6ffd3af6d69af50f4b1f93d
asiwajudev <olanrewajuranda@gmail.com>
2020-07-01 16:19:54 +0100

    Profile View page header completed

--- src/components/profileView/profileView.js
+++ src/components/profileView/profileView.js
@@ -20,9 +20,36 @@ class ProfileView extends Component {
   }
 
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profile" className="btn btn-light mb-3 float-left">
+                Back To Profiles
+              </Link>
+            </div>
+            <div className="col-md-6"></div>
+          </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout />
+          <ProfileCredentials />
+          <ProfileGithub />
+        </div>
+      );
+    }
     return (
       <div>
-        <h1>Test</h1>
+        <div className="profile">
+          <div className="container">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
       </div>
     );
   }
